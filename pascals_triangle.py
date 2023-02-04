19ac4488d1ba3f25a87143d11a327a49984980d5
cmartinezal <cmartinez@teleworx.com>
2023-02-04 11:44:01 +0800

    Code refactor

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -1,34 +1,37 @@
 from typing import List
 
 class PascalTriangle:
-    def generate_rows(self, numRows: int) -> List[List[int]]:
-        if numRows < 1:
-            return  []
+
+    def generate_rows(self, num_rows: int) -> List[List[int]]:
+        pascal_triangle = []
+        
+        if num_rows < 1:
+            return  pascal_triangle
     
-        pascal_triangle = [[1]]
+        pascal_triangle.append([1])
 
-        if numRows == 1:
-            return pascal_triangle[numRows - 1]
+        if num_rows == 1:
+            return pascal_triangle[num_rows - 1]
 
-        current_row = 2
-        while current_row <= numRows:
-            prev_row = pascal_triangle[current_row - 2]
-            row = [prev_row[0]]
+        for row_num in range(2, num_rows + 1):
+            prev_row = pascal_triangle[row_num - 2]
+            new_row = [prev_row[0]]
             last_index = len(prev_row) - 1
+
             for i in range(last_index):
-                row.append(prev_row[i] + prev_row[i + 1])
-            row.append(prev_row[last_index])
-            pascal_triangle.append(row)
-            current_row += 1
+                new_row.append(prev_row[i] + prev_row[i + 1])
+                
+            new_row.append(prev_row[last_index])
+            pascal_triangle.append(new_row)
 
         return pascal_triangle
 
-    def print_PascalTriangle(self, numRows):
-        print(f'Input: numRows = {numRows}')
-        pascal_triangle = self.generate_rows(numRows)
+    def print_PascalTriangle(self, num_rows):
+        print(f'Input: num_rows = {num_rows}')
+        pascal_triangle = self.generate_rows(num_rows)
         print(f'Output: {pascal_triangle}')
 
 # Tests
-PascalTriangle = PascalTriangle()
-for numRows in range(10):
-    PascalTriangle.print_PascalTriangle(numRows)
+pascal_triangle = PascalTriangle()
+for num_rows in range(12):
+    pascal_triangle.print_PascalTriangle(num_rows)
