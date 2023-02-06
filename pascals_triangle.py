5313b8a10d8b0ead6e9d57f483ebc097047aaf21
cmartinezal <cmartinez@teleworx.com>
2023-02-06 15:56:02 +0800

    Code refactor

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -2,25 +2,20 @@ from typing import List
 
 class PascalTriangle:
 
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+
+        prev_row = self.getRow(rowIndex - 1)
+
+        return [1] + [prev_row[i] + prev_row[i + 1] for i in range(0, len(prev_row) -  1) if rowIndex > 1] + [1]
+
     def generate_rows(self, num_rows: int) -> List[List[int]]:
         pascal_triangle = []
+
+        if num_rows > 0:
+            pascal_triangle += [self.getRow(i) for i in range(num_rows)]
         
-        if num_rows < 1:
-            return pascal_triangle
-    
-        pascal_triangle.append([1])
-
-        for row_num in range(2, num_rows + 1):
-            prev_row = pascal_triangle[row_num - 2]
-            new_row = [prev_row[0]]
-            last_index = len(prev_row) - 1
-
-            for i in range(last_index):
-                new_row.append(prev_row[i] + prev_row[i + 1])
-
-            new_row.append(prev_row[last_index])
-            pascal_triangle.append(new_row)
-
         return pascal_triangle
 
     def print_rows(self, num_rows):
