6d2b09c9bd9fcc48e37633d30dcc0f5cd306ae3b
cmartinezal <cmartinez@teleworx.com>
2023-02-07 09:27:21 +0800

    Rename rowIndex to row_index to follow PEP style guide

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -2,13 +2,13 @@ from typing import List
 
 class PascalTriangle:
 
-    def generate_row(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
+    def generate_row(self, row_index: int) -> List[int]:
+        if row_index == 0:
             return [1]
 
-        prev_row = self.generate_row(rowIndex - 1)
+        prev_row = self.generate_row(row_index - 1)
 
-        return [1] + [prev_row[i] + prev_row[i + 1] for i in range(0, len(prev_row) -  1) if rowIndex > 1] + [1]
+        return [1] + [prev_row[i] + prev_row[i + 1] for i in range(0, len(prev_row) -  1) if row_index > 1] + [1]
 
     def generate_pascal_triangle(self, num_rows: int) -> List[List[int]]:
         pascal_triangle = []
