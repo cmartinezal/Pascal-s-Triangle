23c3560665370ec79bfd1004b76ff6ebcc99d19e
cmartinezal <cmartinez@teleworx.com>
2023-04-16 17:30:42 +0800

    Code clean up

--- src/tests/tests_pascal_triangle.py
+++ src/tests/tests_pascal_triangle.py
@@ -2,8 +2,8 @@ import os
 import sys
 root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(root_folder)
-
 from pascal_triangle.pascals_triangle import PascalTriangle
+
 def run_tests():
     pascal_triangle = PascalTriangle()
     for num_rows in range(12):
